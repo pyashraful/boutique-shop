27ac432033e588549d8c8c999ac4234016cc7e4e
Py-Ashraful <ashrafulislam180@gmail.com>
2023-01-26 01:33:00 +0600

    server setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,11 @@
-import express, {
-  Application, Request, Response, NextFunction,
-} from 'express';
-
+import express, { Application, Request, Response, NextFunction } from "express";
 // Boot express
 const app: Application = express();
 const port = 5000;
 
 // Application routing
-app.use('/', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ data: 'Hello from Ornio AS' });
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ data: "Hello from Ornio AS" });
 
   console.log(res);
 });
